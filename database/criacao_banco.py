40e739eee66a48c6a553d4655b79a727ff448987
Gabriel Patara <gabriel.patara@gmail.com>
2024-07-31 10:03:02 -0300

    criacao completa do banco de dados

---
+++ database/criacao_banco.py
@@ -0,0 +1,160 @@
+import mysql.connector
+
+def criar_tabelas(mycursor):
+    # Cria a tabela Tipo_Usuario
+    mycursor.execute("""
+        CREATE TABLE tipo_usuario (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(255) NOT NULL
+        );
+    """)
+    print("Tabela de tipo_usuario foi criada.")
+
+    # Cria a tabela Usuario
+    mycursor.execute("""
+        CREATE TABLE usuario (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome_inteiro VARCHAR(255) NOT NULL,
+            nome_usuario VARCHAR(255) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            senha VARCHAR(255) NOT NULL,
+            tipo INT,
+            FOREIGN KEY (tipo) REFERENCES tipo_usuario(id)
+        );
+    """)
+    print("Tabela de usuários foi criada.")
+
+    # Cria a tabela Artigo
+    mycursor.execute("""
+        CREATE TABLE artigo (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            titulo TEXT,
+            resumo TEXT,
+            introducao TEXT,
+            metodos TEXT,
+            resultados TEXT,
+            discussao TEXT,
+            conclusao TEXT,
+            referencias TEXT,
+            agradecimentos TEXT,
+            informacao_adicional TEXT,
+            usuario_id INT,
+            FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+        );
+    """)
+    print("Tabela de artigos foi criada.")
+
+    # Cria a tabela Especie
+    mycursor.execute("""
+        CREATE TABLE especie (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            titulo TEXT,
+            descricao TEXT,
+            contexto_importancia TEXT,
+            objetivo_artigo TEXT,
+            classificacao_taxonomica TEXT,
+            descricao_classificacao TEXT,
+            reino TEXT,
+            filo TEXT,
+            classe TEXT,
+            ordem TEXT,
+            familia TEXT,
+            genero TEXT,
+            especie TEXT,
+            descricao_especie TEXT,
+            caracteristica_morfologica TEXT,
+            caracteristica_fisiologicas TEXT,
+            caracteristica_comportamental TEXT,
+            distribuicao_geografica TEXT,
+            habitat TEXT,
+            dieta_alimentacao TEXT,
+            predadores_presas TEXT,
+            papel_ecologico TEXT,
+            descricao_processo_reprodutivo TEXT,
+            metodo_reproducao TEXT,
+            ciclo_vida TEXT,
+            cuidados_parentais TEXT,
+            estado_conservacao_especie TEXT,
+            interacoes_humanos TEXT,
+            bibliografia TEXT,
+            usuario_id INT,
+            FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+        );
+    """)
+    print("Tabela de especies foi criada.")
+
+def popular_tabelas(mycursor):
+    # Popula a tabela Tipo_Usuario
+    mycursor.execute("INSERT INTO tipo_usuario (nome) VALUES ('Normal');")
+    mycursor.execute("INSERT INTO tipo_usuario (nome) VALUES ('Colaborador');")
+    print("Tabela de tipo_usuario foi populada.")
+
+    # Popula a tabela Usuario
+    mycursor.execute("""
+        INSERT INTO usuario (nome_inteiro, nome_usuario, email, senha, tipo) VALUES
+        ('João Silva', 'joaos', 'joao@example.com', 'senha123', 1),
+        ('Maria Oliveira', 'mariao', 'maria@example.com', 'senha123', 2);
+    """)
+    print("Tabela de usuários foi populada.")
+
+    # Popula a tabela Artigo
+    mycursor.execute("""
+        INSERT INTO artigo (titulo, resumo, introducao, metodos, resultados, discussao, conclusao, referencias, agradecimentos, informacao_adicional, usuario_id) VALUES
+        ('Estudo sobre a Flora', 'Resumo do estudo', 'Introdução do estudo', 'Métodos do estudo', 'Resultados do estudo', 'Discussão do estudo', 'Conclusão do estudo', 'Referências do estudo', 'Agradecimentos do estudo', 'Informação adicional do estudo', 1),
+        ('Pesquisa sobre a Fauna', 'Resumo da pesquisa', 'Introdução da pesquisa', 'Métodos da pesquisa', 'Resultados da pesquisa', 'Discussão da pesquisa', 'Conclusão da pesquisa', 'Referências da pesquisa', 'Agradecimentos da pesquisa', 'Informação adicional da pesquisa', 2);
+    """)
+    print("Tabela de artigos foi populada.")
+
+    # Popula a tabela Especie
+    mycursor.execute("""
+        INSERT INTO especie (titulo, descricao, contexto_importancia, objetivo_artigo, classificacao_taxonomica, descricao_classificacao, reino, filo, classe, ordem, familia, genero, especie, descricao_especie, caracteristica_morfologica, caracteristica_fisiologicas, caracteristica_comportamental, distribuicao_geografica, habitat, dieta_alimentacao, predadores_presas, papel_ecologico, descricao_processo_reprodutivo, metodo_reproducao, ciclo_vida, cuidados_parentais, estado_conservacao_especie, interacoes_humanos, bibliografia, usuario_id) VALUES
+        ('Especie A', 'Descrição A', 'Importância A', 'Objetivo A', 'Classificação A', 'Descrição Classificação A', 'Reino A', 'Filo A', 'Classe A', 'Ordem A', 'Família A', 'Gênero A', 'Espécie A', 'Descrição Espécie A', 'Morfologia A', 'Fisiologia A', 'Comportamento A', 'Distribuição A', 'Habitat A', 'Dieta A', 'Predadores A', 'Papel Ecológico A', 'Reprodução A', 'Método Reprodução A', 'Ciclo de Vida A', 'Cuidados Parentais A', 'Conservação A', 'Interações Humanos A', 'Bibliografia A', 1),
+        ('Especie B', 'Descrição B', 'Importância B', 'Objetivo B', 'Classificação B', 'Descrição Classificação B', 'Reino B', 'Filo B', 'Classe B', 'Ordem B', 'Família B', 'Gênero B', 'Espécie B', 'Descrição Espécie B', 'Morfologia B', 'Fisiologia B', 'Comportamento B', 'Distribuição B', 'Habitat B', 'Dieta B', 'Predadores B', 'Papel Ecológico B', 'Reprodução B', 'Método Reprodução B', 'Ciclo de Vida B', 'Cuidados Parentais B', 'Conservação B', 'Interações Humanos B', 'Bibliografia B', 2);
+    """)
+    print("Tabela de especies foi populada.")
+
+def main():
+    # Conectar ao banco de dados
+    conn = mysql.connector.connect(
+        host="172.17.0.2",
+        user="root",
+        password="99076641",
+        database="sciconnect"
+    )
+    mycursor = conn.cursor()
+
+    # Verifica se o banco de dados existe
+    mycursor.execute("SHOW DATABASES LIKE %s", ("sciconnect",))
+    result = mycursor.fetchone()
+
+    if result:
+        # Se o banco de dados existe, apaga-o
+        mycursor.execute(f"DROP DATABASE sciconnect;")
+        print(f"Banco de dados sciconnect foi apagado.")
+
+    # Cria o banco de dados
+    mycursor.execute(f"CREATE DATABASE sciconnect")
+    print(f"Banco de dados sciconnect foi criado.")
+
+    # Aponta para o banco de dados
+    mycursor.execute(f"USE sciconnect")
+    print(f"Apontando para o banco de dados.")
+
+    # Criar as tabelas
+    criar_tabelas(mycursor)
+
+    # Commit das transações
+    conn.commit()
+
+    # Popular as tabelas
+    popular_tabelas(mycursor)
+
+    # Commit das transações
+    conn.commit()
+
+    # Fechar a conexão
+    mycursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
--- database/db.sql
+++ database/db.sql
@@ -2,9 +2,90 @@
 USE sciconnect;
 
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(255) UNIQUE,
-    senha VARCHAR(255)
+CREATE TABLE Usuario (
+    Id SERIAL PRIMARY KEY,
+    Nome_inteiro VARCHAR(255) NOT NULL,
+    Nome_usuario VARCHAR(255) UNIQUE NOT NULL,
+    Email VARCHAR(255) UNIQUE NOT NULL,
+    Senha VARCHAR(255) NOT NULL,
+    Tipo INTEGER REFERENCES Tipo_Usuario(Id)
 );
+
+CREATE TABLE Tipo_Usuario (
+    Id SERIAL PRIMARY KEY,
+    Nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Artigo (
+    Id SERIAL PRIMARY KEY,
+    Titulo TEXT,
+    Resumo TEXT,
+    Introducao TEXT,
+    Metodos TEXT,
+    Resultados TEXT,
+    Discussao TEXT,
+    Conclusao TEXT,
+    Referencias TEXT,
+    Agradecimentos TEXT,
+    Informacao_adicional TEXT,
+    Usuario_id INTEGER REFERENCES Usuario(Id)
+);
+
+CREATE TABLE Especie (
+    Id SERIAL PRIMARY KEY,
+    Titulo TEXT,
+    Descricao TEXT,
+    Contexto_importancia TEXT,
+    Objetivo_artigo TEXT,
+    Classificacao_taxonomica TEXT,
+    Descricao_classificacao TEXT,
+    Reino TEXT,
+    Filo TEXT,
+    Classe TEXT,
+    Ordem TEXT,
+    Familia TEXT,
+    Genero TEXT,
+    Especie TEXT,
+    Descricao_especie TEXT,
+    Caracteristica_morfologica TEXT,
+    Caracteristica_fisiologicas TEXT,
+    Caracteristica_comportamental TEXT,
+    Distribuicao_geografica TEXT,
+    Habitat TEXT,
+    Dieta_alimentacao TEXT,
+    Predadores_presas TEXT,
+    Papel_ecologico TEXT,
+    Descricao_processo_reprodutivo TEXT,
+    Metodo_reproducao TEXT,
+    Ciclo_vida TEXT,
+    Cuidados_parentais TEXT,
+    Estado_conservacao_especie TEXT,
+    Interacoes_humanos TEXT,
+    Bibliografia TEXT,
+    Usuario_id INTEGER REFERENCES Usuario(Id)
+);
+
+-- Populando a tabela Tipo_Usuario
+INSERT INTO Tipo_Usuario (Nome) VALUES ('Normal');
+INSERT INTO Tipo_Usuario (Nome) VALUES ('Colaborador');
+
+-- Populando a tabela Usuario
+INSERT INTO Usuario (Nome_inteiro, Nome_usuario, Email, Senha, Tipo) VALUES
+('Alice Silva', 'alice_silva', 'alice@example.com', 'senha123', 1),
+('Bob Santos', 'bob_santos', 'bob@example.com', 'senha123', 2),
+('Carol Costa', 'carol_costa', 'carol@example.com', 'senha123', 1),
+('David Lima', 'david_lima', 'david@example.com', 'senha123', 2);
+
+-- Populando a tabela Artigo
+INSERT INTO Artigo (Titulo, Resumo, Introducao, Metodos, Resultados, Discussao, Conclusao, Referencias, Agradecimentos, Informacao_adicional, Usuario_id) VALUES
+('Estudo sobre a fauna local', 'Resumo do estudo sobre a fauna local', 'Introducao do estudo', 'Metodos utilizados no estudo', 'Resultados do estudo', 'Discussao sobre os resultados', 'Conclusao do estudo', 'Referencias utilizadas', 'Agradecimentos aos colaboradores', 'Informacoes adicionais', 1),
+('Impacto ambiental na floresta', 'Resumo sobre o impacto ambiental', 'Introducao sobre o impacto ambiental', 'Metodos utilizados para avaliar o impacto', 'Resultados da avaliacao', 'Discussao sobre o impacto', 'Conclusao sobre o impacto ambiental', 'Referencias utilizadas', 'Agradecimentos aos colaboradores', 'Informacoes adicionais', 2);
+
+-- Populando a tabela Especie
+INSERT INTO Especie (Titulo, Descricao, Contexto_importancia, Objetivo_artigo, Classificacao_taxonomica, Descricao_classificacao, Reino, Filo, Classe, Ordem, Familia, Genero, Especie, Descricao_especie, Caracteristica_morfologica, Caracteristica_fisiologicas, Caracteristica_comportamental, Distribuicao_geografica, Habitat, Dieta_alimentacao, Predadores_presas, Papel_ecologico, Descricao_processo_reprodutivo, Metodo_reproducao, Ciclo_vida, Cuidados_parentais, Estado_conservacao_especie, Interacoes_humanos, Bibliografia, Usuario_id) VALUES
+('Estudo sobre a espécie A', 'Descrição da espécie A', 'Importância da espécie A', 'Objetivo do artigo sobre a espécie A', 'Classificação taxonômica da espécie A', 'Descrição da classificação da espécie A', 'Animalia', 'Chordata', 'Mammalia', 'Carnivora', 'Felidae', 'Panthera', 'Panthera leo', 'Descrição do leão', 'Características morfológicas do leão', 'Características fisiológicas do leão', 'Comportamento do leão', 'Distribuição geográfica do leão', 'Habitat do leão', 'Dieta do leão', 'Predadores e presas do leão', 'Papel ecológico do leão', 'Descrição do processo reprodutivo do leão', 'Método de reprodução do leão', 'Ciclo de vida do leão', 'Cuidados parentais do leão', 'Estado de conservação do leão', 'Interações do leão com humanos', 'Bibliografia sobre o leão', 1),
+('Estudo sobre a espécie B', 'Descrição da espécie B', 'Importância da espécie B', 'Objetivo do artigo sobre a espécie B', 'Classificação taxonômica da espécie B', 'Descrição da classificação da espécie B', 'Plantae', 'Magnoliophyta', 'Magnoliopsida', 'Rosales', 'Rosaceae', 'Rosa', 'Rosa gallica', 'Descrição da rosa gallica', 'Características morfológicas da rosa gallica', 'Características fisiológicas da rosa gallica', 'Comportamento da rosa gallica', 'Distribuição geográfica da rosa gallica', 'Habitat da rosa gallica', 'Dieta da rosa gallica', 'Predadores e presas da rosa gallica', 'Papel ecológico da rosa gallica', 'Descrição do processo reprodutivo da rosa gallica', 'Método de reprodução da rosa gallica', 'Ciclo de vida da rosa gallica', 'Cuidados parentais da rosa gallica', 'Estado de conservação da rosa gallica', 'Interações da rosa gallica com humanos', 'Bibliografia sobre a rosa gallica', 2);
+
+-- Confirmando as inserções
+COMMIT;
 
